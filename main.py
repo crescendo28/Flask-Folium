3bc9119e214eca3b86abd5b67351bd3bb21c88e6
crescent <82457785+crescendo28@users.noreply.github.com>
2024-11-25 11:27:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_the_map():
-    return render_template('ina.html')
+    return render_template("ina.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
